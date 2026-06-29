89ccb146d067aee8cae47c60e393c541b45e90b4
kdnth <kadenconradt@gmail.com>
2026-06-29 17:20:29 -0400

    Initial commit

---
+++ src/main/java/com/kdnth/campaignkeep/CampaignkeepApplication.java
@@ -0,0 +1,13 @@
+package com.kdnth.campaignkeep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampaignkeepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampaignkeepApplication.class, args);
+	}
+
+}
